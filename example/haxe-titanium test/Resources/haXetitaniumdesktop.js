85371465d41b520ec453aae4a6290444b5c4cb18
Skial Bainn <skial.bainn@gmail.com>
2011-02-24 18:53:28 +0000

    minor

---
+++ example/haxe-titanium test/Resources/haXetitaniumdesktop.js
@@ -0,0 +1,725 @@
+$estr = function() { return js.Boot.__string_rec(this,''); }
+IntIter = function(min,max) { if( min === $_ ) return; {
+	$s.push("IntIter::new");
+	var $spos = $s.length;
+	this.min = min;
+	this.max = max;
+	$s.pop();
+}}
+IntIter.__name__ = ["IntIter"];
+IntIter.prototype.hasNext = function() {
+	$s.push("IntIter::hasNext");
+	var $spos = $s.length;
+	{
+		var $tmp = this.min < this.max;
+		$s.pop();
+		return $tmp;
+	}
+	$s.pop();
+}
+IntIter.prototype.max = null;
+IntIter.prototype.min = null;
+IntIter.prototype.next = function() {
+	$s.push("IntIter::next");
+	var $spos = $s.length;
+	{
+		var $tmp = this.min++;
+		$s.pop();
+		return $tmp;
+	}
+	$s.pop();
+}
+IntIter.prototype.__class__ = IntIter;
+if(typeof js=='undefined') js = {}
+js.Boot = function() { }
+js.Boot.__name__ = ["js","Boot"];
+js.Boot.__unhtml = function(s) {
+	$s.push("js.Boot::__unhtml");
+	var $spos = $s.length;
+	{
+		var $tmp = s.split("&").join("&amp;").split("<").join("&lt;").split(">").join("&gt;");
+		$s.pop();
+		return $tmp;
+	}
+	$s.pop();
+}
+js.Boot.__trace = function(v,i) {
+	$s.push("js.Boot::__trace");
+	var $spos = $s.length;
+	var msg = (i != null?((i.fileName + ":") + i.lineNumber) + ": ":"");
+	msg += js.Boot.__unhtml(js.Boot.__string_rec(v,"")) + "<br/>";
+	var d = document.getElementById("haxe:trace");
+	if(d == null) alert("No haxe:trace element defined\n" + msg);
+	else d.innerHTML += msg;
+	$s.pop();
+}
+js.Boot.__clear_trace = function() {
+	$s.push("js.Boot::__clear_trace");
+	var $spos = $s.length;
+	var d = document.getElementById("haxe:trace");
+	if(d != null) d.innerHTML = "";
+	else null;
+	$s.pop();
+}
+js.Boot.__closure = function(o,f) {
+	$s.push("js.Boot::__closure");
+	var $spos = $s.length;
+	var m = o[f];
+	if(m == null) {
+		$s.pop();
+		return null;
+	}
+	var f1 = function() {
+		$s.push("js.Boot::__closure@67");
+		var $spos = $s.length;
+		{
+			var $tmp = m.apply(o,arguments);
+			$s.pop();
+			return $tmp;
+		}
+		$s.pop();
+	}
+	f1.scope = o;
+	f1.method = m;
+	{
+		$s.pop();
+		return f1;
+	}
+	$s.pop();
+}
+js.Boot.__string_rec = function(o,s) {
+	$s.push("js.Boot::__string_rec");
+	var $spos = $s.length;
+	if(o == null) {
+		$s.pop();
+		return "null";
+	}
+	if(s.length >= 5) {
+		$s.pop();
+		return "<...>";
+	}
+	var t = typeof(o);
+	if(t == "function" && (o.__name__ != null || o.__ename__ != null)) t = "object";
+	switch(t) {
+	case "object":{
+		if(o instanceof Array) {
+			if(o.__enum__ != null) {
+				if(o.length == 2) {
+					var $tmp = o[0];
+					$s.pop();
+					return $tmp;
+				}
+				var str = o[0] + "(";
+				s += "\t";
+				{
+					var _g1 = 2, _g = o.length;
+					while(_g1 < _g) {
+						var i = _g1++;
+						if(i != 2) str += "," + js.Boot.__string_rec(o[i],s);
+						else str += js.Boot.__string_rec(o[i],s);
+					}
+				}
+				{
+					var $tmp = str + ")";
+					$s.pop();
+					return $tmp;
+				}
+			}
+			var l = o.length;
+			var i;
+			var str = "[";
+			s += "\t";
+			{
+				var _g = 0;
+				while(_g < l) {
+					var i1 = _g++;
+					str += ((i1 > 0?",":"")) + js.Boot.__string_rec(o[i1],s);
+				}
+			}
+			str += "]";
+			{
+				$s.pop();
+				return str;
+			}
+		}
+		var tostr;
+		try {
+			tostr = o.toString;
+		}
+		catch( $e0 ) {
+			{
+				var e = $e0;
+				{
+					$e = [];
+					while($s.length >= $spos) $e.unshift($s.pop());
+					$s.push($e[0]);
+					{
+						$s.pop();
+						return "???";
+					}
+				}
+			}
+		}
+		if(tostr != null && tostr != Object.toString) {
+			var s2 = o.toString();
+			if(s2 != "[object Object]") {
+				$s.pop();
+				return s2;
+			}
+		}
+		var k = null;
+		var str = "{\n";
+		s += "\t";
+		var hasp = (o.hasOwnProperty != null);
+		for( var k in o ) { ;
+		if(hasp && !o.hasOwnProperty(k)) continue;
+		if(k == "prototype" || k == "__class__" || k == "__super__" || k == "__interfaces__") continue;
+		if(str.length != 2) str += ", \n";
+		str += ((s + k) + " : ") + js.Boot.__string_rec(o[k],s);
+		}
+		s = s.substring(1);
+		str += ("\n" + s) + "}";
+		{
+			$s.pop();
+			return str;
+		}
+	}break;
+	case "function":{
+		{
+			$s.pop();
+			return "<function>";
+		}
+	}break;
+	case "string":{
+		{
+			$s.pop();
+			return o;
+		}
+	}break;
+	default:{
+		{
+			var $tmp = String(o);
+			$s.pop();
+			return $tmp;
+		}
+	}break;
+	}
+	$s.pop();
+}
+js.Boot.__interfLoop = function(cc,cl) {
+	$s.push("js.Boot::__interfLoop");
+	var $spos = $s.length;
+	if(cc == null) {
+		$s.pop();
+		return false;
+	}
+	if(cc == cl) {
+		$s.pop();
+		return true;
+	}
+	var intf = cc.__interfaces__;
+	if(intf != null) {
+		var _g1 = 0, _g = intf.length;
+		while(_g1 < _g) {
+			var i = _g1++;
+			var i1 = intf[i];
+			if(i1 == cl || js.Boot.__interfLoop(i1,cl)) {
+				$s.pop();
+				return true;
+			}
+		}
+	}
+	{
+		var $tmp = js.Boot.__interfLoop(cc.__super__,cl);
+		$s.pop();
+		return $tmp;
+	}
+	$s.pop();
+}
+js.Boot.__instanceof = function(o,cl) {
+	$s.push("js.Boot::__instanceof");
+	var $spos = $s.length;
+	try {
+		if(o instanceof cl) {
+			if(cl == Array) {
+				var $tmp = (o.__enum__ == null);
+				$s.pop();
+				return $tmp;
+			}
+			{
+				$s.pop();
+				return true;
+			}
+		}
+		if(js.Boot.__interfLoop(o.__class__,cl)) {
+			$s.pop();
+			return true;
+		}
+	}
+	catch( $e0 ) {
+		{
+			var e = $e0;
+			{
+				$e = [];
+				while($s.length >= $spos) $e.unshift($s.pop());
+				$s.push($e[0]);
+				if(cl == null) {
+					$s.pop();
+					return false;
+				}
+			}
+		}
+	}
+	switch(cl) {
+	case Int:{
+		{
+			var $tmp = Math.ceil(o%2147483648.0) === o;
+			$s.pop();
+			return $tmp;
+		}
+	}break;
+	case Float:{
+		{
+			var $tmp = typeof(o) == "number";
+			$s.pop();
+			return $tmp;
+		}
+	}break;
+	case Bool:{
+		{
+			var $tmp = o === true || o === false;
+			$s.pop();
+			return $tmp;
+		}
+	}break;
+	case String:{
+		{
+			var $tmp = typeof(o) == "string";
+			$s.pop();
+			return $tmp;
+		}
+	}break;
+	case Dynamic:{
+		{
+			$s.pop();
+			return true;
+		}
+	}break;
+	default:{
+		if(o == null) {
+			$s.pop();
+			return false;
+		}
+		{
+			var $tmp = o.__enum__ == cl || (cl == Class && o.__name__ != null) || (cl == Enum && o.__ename__ != null);
+			$s.pop();
+			return $tmp;
+		}
+	}break;
+	}
+	$s.pop();
+}
+js.Boot.__init = function() {
+	$s.push("js.Boot::__init");
+	var $spos = $s.length;
+	js.Lib.isIE = (typeof document!='undefined' && document.all != null && typeof window!='undefined' && window.opera == null);
+	js.Lib.isOpera = (typeof window!='undefined' && window.opera != null);
+	Array.prototype.copy = Array.prototype.slice;
+	Array.prototype.insert = function(i,x) {
+		$s.push("js.Boot::__init@205");
+		var $spos = $s.length;
+		this.splice(i,0,x);
+		$s.pop();
+	}
+	Array.prototype.remove = (Array.prototype.indexOf?function(obj) {
+		$s.push("js.Boot::__init@208");
+		var $spos = $s.length;
+		var idx = this.indexOf(obj);
+		if(idx == -1) {
+			$s.pop();
+			return false;
+		}
+		this.splice(idx,1);
+		{
+			$s.pop();
+			return true;
+		}
+		$s.pop();
+	}:function(obj) {
+		$s.push("js.Boot::__init@213");
+		var $spos = $s.length;
+		var i = 0;
+		var l = this.length;
+		while(i < l) {
+			if(this[i] == obj) {
+				this.splice(i,1);
+				{
+					$s.pop();
+					return true;
+				}
+			}
+			i++;
+		}
+		{
+			$s.pop();
+			return false;
+		}
+		$s.pop();
+	});
+	Array.prototype.iterator = function() {
+		$s.push("js.Boot::__init@225");
+		var $spos = $s.length;
+		{
+			var $tmp = { cur : 0, arr : this, hasNext : function() {
+				$s.push("js.Boot::__init@225@229");
+				var $spos = $s.length;
+				{
+					var $tmp = this.cur < this.arr.length;
+					$s.pop();
+					return $tmp;
+				}
+				$s.pop();
+			}, next : function() {
+				$s.push("js.Boot::__init@225@232");
+				var $spos = $s.length;
+				{
+					var $tmp = this.arr[this.cur++];
+					$s.pop();
+					return $tmp;
+				}
+				$s.pop();
+			}}
+			$s.pop();
+			return $tmp;
+		}
+		$s.pop();
+	}
+	String.prototype.cca = String.prototype.charCodeAt;
+	String.prototype.charCodeAt = function(i) {
+		$s.push("js.Boot::__init@238");
+		var $spos = $s.length;
+		var x = this.cca(i);
+		if(x != x) {
+			$s.pop();
+			return null;
+		}
+		{
+			$s.pop();
+			return x;
+		}
+		$s.pop();
+	}
+	var oldsub = String.prototype.substr;
+	String.prototype.substr = function(pos,len) {
+		$s.push("js.Boot::__init@245");
+		var $spos = $s.length;
+		if(pos != null && pos != 0 && len != null && len < 0) {
+			$s.pop();
+			return "";
+		}
+		if(len == null) len = this.length;
+		if(pos < 0) {
+			pos = this.length + pos;
+			if(pos < 0) pos = 0;
+		}
+		else if(len < 0) {
+			len = (this.length + len) - pos;
+		}
+		{
+			var $tmp = oldsub.apply(this,[pos,len]);
+			$s.pop();
+			return $tmp;
+		}
+		$s.pop();
+	}
+	$closure = js.Boot.__closure;
+	$s.pop();
+}
+js.Boot.prototype.__class__ = js.Boot;
+if(typeof haxe=='undefined') haxe = {}
+haxe.Log = function() { }
+haxe.Log.__name__ = ["haxe","Log"];
+haxe.Log.trace = function(v,infos) {
+	$s.push("haxe.Log::trace");
+	var $spos = $s.length;
+	js.Boot.__trace(v,infos);
+	$s.pop();
+}
+haxe.Log.clear = function() {
+	$s.push("haxe.Log::clear");
+	var $spos = $s.length;
+	js.Boot.__clear_trace();
+	$s.pop();
+}
+haxe.Log.prototype.__class__ = haxe.Log;
+Std = function() { }
+Std.__name__ = ["Std"];
+Std["is"] = function(v,t) {
+	$s.push("Std::is");
+	var $spos = $s.length;
+	{
+		var $tmp = js.Boot.__instanceof(v,t);
+		$s.pop();
+		return $tmp;
+	}
+	$s.pop();
+}
+Std.string = function(s) {
+	$s.push("Std::string");
+	var $spos = $s.length;
+	{
+		var $tmp = js.Boot.__string_rec(s,"");
+		$s.pop();
+		return $tmp;
+	}
+	$s.pop();
+}
+Std["int"] = function(x) {
+	$s.push("Std::int");
+	var $spos = $s.length;
+	if(x < 0) {
+		var $tmp = Math.ceil(x);
+		$s.pop();
+		return $tmp;
+	}
+	{
+		var $tmp = Math.floor(x);
+		$s.pop();
+		return $tmp;
+	}
+	$s.pop();
+}
+Std.parseInt = function(x) {
+	$s.push("Std::parseInt");
+	var $spos = $s.length;
+	var v = parseInt(x,10);
+	if(v == 0 && x.charCodeAt(1) == 120) v = parseInt(x);
+	if(isNaN(v)) {
+		$s.pop();
+		return null;
+	}
+	{
+		var $tmp = v;
+		$s.pop();
+		return $tmp;
+	}
+	$s.pop();
+}
+Std.parseFloat = function(x) {
+	$s.push("Std::parseFloat");
+	var $spos = $s.length;
+	{
+		var $tmp = parseFloat(x);
+		$s.pop();
+		return $tmp;
+	}
+	$s.pop();
+}
+Std.random = function(x) {
+	$s.push("Std::random");
+	var $spos = $s.length;
+	{
+		var $tmp = Math.floor(Math.random() * x);
+		$s.pop();
+		return $tmp;
+	}
+	$s.pop();
+}
+Std.prototype.__class__ = Std;
+Main = function() { }
+Main.__name__ = ["Main"];
+Main.main = function() {
+	$s.push("Main::main");
+	var $spos = $s.length;
+	haxe.Log.trace("This epic app was made with Titanium Desktop haXe wrappers by Skial Bainn",{ fileName : "Main.hx", lineNumber : 14, className : "Main", methodName : "main"});
+	haxe.Log.trace("platform: " + Titanium.getPlatform(),{ fileName : "Main.hx", lineNumber : 15, className : "Main", methodName : "main"});
+	haxe.Log.trace("version: " + Titanium.getVersion(),{ fileName : "Main.hx", lineNumber : 16, className : "Main", methodName : "main"});
+	var window = Titanium.UI.getCurrentWindow();
+	haxe.Log.trace("current window height: " + window.getHeight(),{ fileName : "Main.hx", lineNumber : 18, className : "Main", methodName : "main"});
+	haxe.Log.trace("current window width: " + window.getWidth(),{ fileName : "Main.hx", lineNumber : 19, className : "Main", methodName : "main"});
+	haxe.Log.trace("current window x: " + window.getX(),{ fileName : "Main.hx", lineNumber : 20, className : "Main", methodName : "main"});
+	haxe.Log.trace("current window y: " + window.getY(),{ fileName : "Main.hx", lineNumber : 21, className : "Main", methodName : "main"});
+	haxe.Log.trace("operating system name: " + Titanium.Platform.name,{ fileName : "Main.hx", lineNumber : 22, className : "Main", methodName : "main"});
+	haxe.Log.trace("opertating system type: " + Titanium.Platform.ostype,{ fileName : "Main.hx", lineNumber : 23, className : "Main", methodName : "main"});
+	haxe.Log.trace("host mac address: " + Titanium.Platform.macaddress,{ fileName : "Main.hx", lineNumber : 24, className : "Main", methodName : "main"});
+	haxe.Log.trace("host ip address: " + Titanium.Platform.address,{ fileName : "Main.hx", lineNumber : 25, className : "Main", methodName : "main"});
+	haxe.Log.trace("host processor count: " + Titanium.Platform.processorCount,{ fileName : "Main.hx", lineNumber : 26, className : "Main", methodName : "main"});
+	$s.pop();
+}
+Main.prototype.__class__ = Main;
+js.Lib = function() { }
+js.Lib.__name__ = ["js","Lib"];
+js.Lib.isIE = null;
+js.Lib.isOpera = null;
+js.Lib.document = null;
+js.Lib.window = null;
+js.Lib.alert = function(v) {
+	$s.push("js.Lib::alert");
+	var $spos = $s.length;
+	alert(js.Boot.__string_rec(v,""));
+	$s.pop();
+}
+js.Lib.eval = function(code) {
+	$s.push("js.Lib::eval");
+	var $spos = $s.length;
+	{
+		var $tmp = eval(code);
+		$s.pop();
+		return $tmp;
+	}
+	$s.pop();
+}
+js.Lib.setErrorHandler = function(f) {
+	$s.push("js.Lib::setErrorHandler");
+	var $spos = $s.length;
+	js.Lib.onerror = f;
+	$s.pop();
+}
+js.Lib.prototype.__class__ = js.Lib;
+$_ = {}
+js.Boot.__res = {}
+$s = [];
+$e = [];
+js.Boot.__init();
+{
+	Math.__name__ = ["Math"];
+	Math.NaN = Number["NaN"];
+	Math.NEGATIVE_INFINITY = Number["NEGATIVE_INFINITY"];
+	Math.POSITIVE_INFINITY = Number["POSITIVE_INFINITY"];
+	Math.isFinite = function(i) {
+		$s.push("js.Lib::setErrorHandler");
+		var $spos = $s.length;
+		{
+			var $tmp = isFinite(i);
+			$s.pop();
+			return $tmp;
+		}
+		$s.pop();
+	}
+	Math.isNaN = function(i) {
+		$s.push("js.Lib::setErrorHandler");
+		var $spos = $s.length;
+		{
+			var $tmp = isNaN(i);
+			$s.pop();
+			return $tmp;
+		}
+		$s.pop();
+	}
+}
+{
+	String.prototype.__class__ = String;
+	String.__name__ = ["String"];
+	Array.prototype.__class__ = Array;
+	Array.__name__ = ["Array"];
+	Int = { __name__ : ["Int"]}
+	Dynamic = { __name__ : ["Dynamic"]}
+	Float = Number;
+	Float.__name__ = ["Float"];
+	Bool = { __ename__ : ["Bool"]}
+	Class = { __name__ : ["Class"]}
+	Enum = { }
+	Void = { __ename__ : ["Void"]}
+}
+{
+	js.Lib.document = document;
+	js.Lib.window = window;
+	onerror = function(msg,url,line) {
+		var stack = $s.copy();
+		var f = js.Lib.onerror;
+		$s.splice(0,$s.length);
+		if( f == null ) {
+			var i = stack.length;
+			var s = "";
+			while( --i >= 0 )
+				s += "Called from "+stack[i]+"\n";
+			alert(msg+"\n\n"+s);
+			return false;
+		}
+		return f(msg,stack);
+	}
+}
+{
+	var d = Date;
+	d.now = function() {
+		$s.push("js.Lib::setErrorHandler");
+		var $spos = $s.length;
+		{
+			var $tmp = new Date();
+			$s.pop();
+			return $tmp;
+		}
+		$s.pop();
+	}
+	d.fromTime = function(t) {
+		$s.push("js.Lib::setErrorHandler");
+		var $spos = $s.length;
+		var d1 = new Date();
+		d1["setTime"](t);
+		{
+			$s.pop();
+			return d1;
+		}
+		$s.pop();
+	}
+	d.fromString = function(s) {
+		$s.push("js.Lib::setErrorHandler");
+		var $spos = $s.length;
+		switch(s.length) {
+		case 8:{
+			var k = s.split(":");
+			var d1 = new Date();
+			d1["setTime"](0);
+			d1["setUTCHours"](k[0]);
+			d1["setUTCMinutes"](k[1]);
+			d1["setUTCSeconds"](k[2]);
+			{
+				$s.pop();
+				return d1;
+			}
+		}break;
+		case 10:{
+			var k = s.split("-");
+			{
+				var $tmp = new Date(k[0],k[1] - 1,k[2],0,0,0);
+				$s.pop();
+				return $tmp;
+			}
+		}break;
+		case 19:{
+			var k = s.split(" ");
+			var y = k[0].split("-");
+			var t = k[1].split(":");
+			{
+				var $tmp = new Date(y[0],y[1] - 1,y[2],t[0],t[1],t[2]);
+				$s.pop();
+				return $tmp;
+			}
+		}break;
+		default:{
+			throw "Invalid date format : " + s;
+		}break;
+		}
+		$s.pop();
+	}
+	d.prototype["toString"] = function() {
+		$s.push("js.Lib::setErrorHandler");
+		var $spos = $s.length;
+		var date = this;
+		var m = date.getMonth() + 1;
+		var d1 = date.getDate();
+		var h = date.getHours();
+		var mi = date.getMinutes();
+		var s = date.getSeconds();
+		{
+			var $tmp = (((((((((date.getFullYear() + "-") + ((m < 10?"0" + m:"" + m))) + "-") + ((d1 < 10?"0" + d1:"" + d1))) + " ") + ((h < 10?"0" + h:"" + h))) + ":") + ((mi < 10?"0" + mi:"" + mi))) + ":") + ((s < 10?"0" + s:"" + s));
+			$s.pop();
+			return $tmp;
+		}
+		$s.pop();
+	}
+	d.prototype.__class__ = d;
+	d.__name__ = ["Date"];
+}
+js.Lib.onerror = null;
+Main.main()
